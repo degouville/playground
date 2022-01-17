5cd7bdefe3d054558af5c1edd8764d1ac322cf5e
Mathieu de Gouville <mathieu@degouville.com>
2022-01-17 13:42:40 +0100

    Add Responsive Demo Project

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script>
+import BlogCard from './components/BlogCard.vue'
+
+export default {
+  components: {
+    BlogCard
+  },
+
+  data() {
+    return {
+      name: 'Weekly Roundup',
+      category: 'NFT',
+      image: 'https://academy-public.coinmarketcap.com/optimized-uploads/b4a1f7607ba046ca8f96acc1e3ebd122.jpg',
+      author: 'Mathieu de Gouville',
+      desc: `Steph Curry Purchases LinksDAO NFT, Pushes Price to New Highs`,
+      link: 'https://coinmarketcap.com/alexandria/article/weekly-roundup-what-happened-in-play2earn-games-and-nfts-this-week-jan-14-2022'
+    }
+  },
+
+  methods: {
+    async fetchData() {
+      // NOTE: 1/2 API Request should look like this
+      // const { name, category, image, author, desc, link } = await axios.get(`https://api.com/articles/last`)
+
+      // NOTE: 2/2 but we're using a mock API
+      const { name, category, image, author, desc, link } = {
+        name: 'Weekly Roundup',
+        category: 'NFT',
+        image: 'https://academy-public.coinmarketcap.com/optimized-uploads/b4a1f7607ba046ca8f96acc1e3ebd122.jpg',
+        author: 'Mathieu de Gouville',
+        desc: `Steph Curry Purchases LinksDAO NFT, Pushes Price to New Highs`,
+        link: 'https://coinmarketcap.com/alexandria/article/weekly-roundup-what-happened-in-play2earn-games-and-nfts-this-week-jan-14-2022'
+      }
+
+      this.name = name
+      this.category = category
+      this.image = image
+      this.author = author
+      this.desc = desc
+      this.link = link
+    }
+  },
+
+  created() {
+    this.fetchData()
+  }
+}
+</script>
+
+<template lang="pug">
+#container
+  blog-card(
+    :name="name"
+    :category="category"
+    :image="image"
+    :author="author"
+    :desc="desc"
+    :link="link"
+  )
+</template>
+
+<style lang="stylus">
+@import url("https://fonts.googleapis.com/css?family=Lato:400,700")
+*
+  box-sizing: border-box
+
+  &::before, 
+  &::after
+    box-sizing: border-box
+
+body
+  margin 0
+  padding 0
+
+  #app
+    display flex
+    font-family "Lato", sans-serif
+    font-weight 400
+    color #777
+    background #ccd8ee
+    font-size .9rem
+    min-height 100vh
+    margin 0
+    line-height 1.6
+    align-items center
+    justify-content center
+    text-rendering optimizeLegibility
+
+    #container
+      width 30rem
+      height 13.625rem
+</style>
---
+++ src/components/BlogCard.vue
@@ -0,0 +1,117 @@
+<script>
+export default {
+  props: {
+    name: String,
+    category: String,
+    image: String,
+    author: String,
+    desc: String,
+    link: String,
+  }
+}
+</script>
+
+<template lang="pug">
+a.card-link(:href="link")
+  article.blog-card 
+    img.post-image(:src="image")
+    .article-details 
+      h4.post-category {{ category }}
+      h3.post-title {{ name }}
+      p.post-description {{ desc }}
+      p.post-author By {{ author }}
+</template>
+
+<style lang="stylus" scoped>
+.card-link
+  position relative
+  display block
+  color inherit
+  text-decoration none
+
+  &:hover
+    .post-title
+      transition color 0.3s ease
+      color #e04f62
+
+  &:hover
+    .post-image
+      transition opacity 0.3s ease
+      opacity 0.9
+
+.blog-card
+  display flex
+  flex-direction row
+  background #fff
+  box-shadow 0 0.1875rem 1.5rem rgba(0, 0, 0, 0.2)
+  border-radius 0.375rem
+  overflow hidden
+
+.post-image
+  transition opacity 0.3s ease
+  display block
+  width 100%
+  object-fit cover
+
+.article-details
+  padding 1.5rem
+
+.post-category
+  display inline-block
+  text-transform uppercase
+  font-size 0.75rem
+  font-weight 700
+  line-height 1
+  letter-spacing 0.0625rem
+  margin 0 0 0.75rem 0
+  padding 0 0 0.25rem 0
+  border-bottom 0.125rem solid #ebebeb
+
+.post-title
+  transition color 0.3s ease
+  font-size 1.125rem
+  line-height 1.4
+  color #121212
+  font-weight 700
+  margin 0 0 0.5rem 0
+
+.post-author
+  font-size 0.875rem
+  line-height 1
+  margin 1.125rem 0 0 0
+  padding 1.125rem 0 0 0
+  border-top 0.0625rem solid #ebebeb
+
+@media(max-width 40rem)
+  #container
+    width 18rem
+    height 27.25rem
+  
+  .blog-card
+    flex-wrap wrap
+  
+@supports(display grid)
+  #app
+    display grid
+    grid-template-columns repeat(4, 1fr)
+    grid-gap 0.625rem
+    grid-template-areas ". main main ." ". main main ."
+  
+  #container
+    grid-area main
+    align-self center
+    justify-self center
+
+  .post-image
+    height 100%
+
+  .blog-card
+    display grid
+    grid-template-columns 1fr 2fr
+    grid-template-rows 1fr
+
+  @media (max-width 40rem)
+    .blog-card
+      grid-template-columns auto
+      grid-template-rows 12rem 1fr
+</style>
